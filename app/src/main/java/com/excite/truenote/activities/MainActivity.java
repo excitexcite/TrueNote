4c7109df9feddb0acb48d596bd1240e2ff695095
excitexcite <excitenew@gmail.com>
2021-05-04 20:23:06 +0300

    Implemnent ability to show notes

--- app/src/main/java/com/excite/truenote/activities/MainActivity.java
+++ app/src/main/java/com/excite/truenote/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.excite.truenote.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -11,14 +14,20 @@ import android.widget.ImageView;
 
 import com.excite.truenote.R;
 import com.excite.truenote.activities.CreateNoteActivity;
+import com.excite.truenote.adapters.NotesAdapter;
 import com.excite.truenote.database.NotesDatabase;
 import com.excite.truenote.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private RecyclerView notesRecyclerView;
+    private List<Note> mNoteList;
+    private NotesAdapter mNotesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+        );
+        
+        mNoteList = new ArrayList<>();
+        mNotesAdapter = new NotesAdapter(mNoteList);
+        notesRecyclerView.setAdapter(mNotesAdapter);
+        
         getNotes();
     }
 
@@ -54,7 +72,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MYNOTES", notes.toString());
+                //Log.d("MYNOTES", notes.toString());
+                // если список пусть, значит осуществляется первый запуск приложения, то есть первый запрос к бд
+                if (mNoteList.size() == 0) {
+                    mNoteList.addAll(notes);
+                    mNotesAdapter.notifyDataSetChanged();
+                } else { // иначе приложение уже было запущено и необходимо лишь отобразить в списке добавленную заметку в начале списка
+                    mNoteList.add(0, notes.get(0));
+                    mNotesAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
         }
 
@@ -62,4 +89,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // без этой функции при создании заметки и возврата на главный экран созданная заметка не будет отображена
+    // это связано с тем, что используется startActivityForResult() 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+        }
+    }
 }
---
+++ app/src/main/java/com/excite/truenote/adapters/NotesAdapter.java
@@ -0,0 +1,75 @@
+package com.excite.truenote.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.excite.truenote.R;
+import com.excite.truenote.entities.Note;
+
+import java.util.List;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+
+    private List<Note> mNotes;
+
+    public NotesAdapter(List<Note> notes) {
+        mNotes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.item_container_note,
+                        parent,
+                        false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.setNote(mNotes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textTitle;
+        TextView textSubtitle;
+        TextView textDateTime;
+
+        NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textSubtitle = itemView.findViewById(R.id.textSubtitle);
+            textDateTime = itemView.findViewById(R.id.textDateTime);
+        }
+
+        void setNote(Note note) {
+            textTitle.setText(note.getTitle());
+            if (note.getSubtitle().trim().isEmpty()) {
+                textSubtitle.setVisibility(View.GONE);
+            } else {
+                textSubtitle.setText(note.getSubtitle());
+            }
+            textDateTime.setText(note.getDateTime());
+        }
+
+    }
+}
