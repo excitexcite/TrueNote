e6213d6c09eafda7e3ae7b481019e758afea558f
excitexcite <excitenew@gmail.com>
2021-05-04 19:38:11 +0300

    Add function to add notes to database.

--- app/src/main/java/com/excite/truenote/activities/CreateNoteActivity.java
+++ app/src/main/java/com/excite/truenote/activities/CreateNoteActivity.java
@@ -2,13 +2,29 @@ package com.excite.truenote.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.excite.truenote.R;
+import com.excite.truenote.database.NotesDatabase;
+import com.excite.truenote.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle;
+    private EditText inputNoteSubtitle;
+    private EditText inputNoteText;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +38,64 @@ public class CreateNoteActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNote);
+        textDateTime = findViewById(R.id.textDateTime);
+
+        textDateTime.setText(
+                // ex: Tuesday, 04 May 2021 16:42 PM
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).
+                        format(new Date())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+    }
+
+    private void saveNote() {
+        if (inputNoteTitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty!", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty()
+                && inputNoteText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note can't be empty!", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString().trim());
+        note.setSubtitle(inputNoteSubtitle.getText().toString().trim());
+        note.setNoteText(inputNoteText.getText().toString().trim());
+        note.setDateTime(textDateTime.getText().toString());
+
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getDatabase(getApplicationContext())
+                        .noteDao()
+                        .insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+
+        new SaveNoteTask().execute();
+
     }
 }
--- app/src/main/java/com/excite/truenote/activities/MainActivity.java
+++ app/src/main/java/com/excite/truenote/activities/MainActivity.java
@@ -3,12 +3,18 @@ package com.excite.truenote.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.excite.truenote.R;
 import com.excite.truenote.activities.CreateNoteActivity;
+import com.excite.truenote.database.NotesDatabase;
+import com.excite.truenote.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +35,31 @@ public class MainActivity extends AppCompatActivity {
                 );
             }
         });
+
+        getNotes();
     }
+
+    private void getNotes() {
+
+        class GetNoteTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase
+                        .getDatabase(getApplicationContext())
+                        .noteDao()
+                        .getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MYNOTES", notes.toString());
+            }
+        }
+
+        new GetNoteTask().execute();
+
+    }
+
 }
